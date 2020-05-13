79af71d450901cf3d144b95f81582543e60b8ad9
angeloaapp <angelo-sc1@hotmail.com>
2020-05-13 17:32:44 -0300

    implementação textToSpeetch

--- projetoWatson/lib/ibmWatsonCredentials.js
+++ projetoWatson/lib/ibmWatsonCredentials.js
@@ -1,4 +1,5 @@
 const AssistantV1 = require('ibm-watson/assistant/v1');
+const TextToSpeechV1 = require('ibm-watson/text-to-speech/v1');
 const {IamAuthenticator} = require('ibm-watson/auth');
 
 //configuração para IBM-Watson Assistsnt
@@ -7,5 +8,10 @@ const assistant = new AssistantV1({
     version: '2020-01-04',
     authenticator: new IamAuthenticator({ apikey: 'BMWXLNMecMjY-tIoBIY61bjmGRQi1PNneNhVwh3HX8wt'})
 }); 
+//configuração para o IBM Watson Text to Speech
+const textToSpeech = new TextToSpeechV1({
+    authenticator: new IamAuthenticator({apikey: 'o6DFad_6gcFjYNzsW9-77LDZNRMcBXX1Rl-g4UE9wYyV'}),
+    url: 'https://stream.watsonplatform.net/text-to-speech/api/'
+});
 
-module.exports = {assistant};
+module.exports = {assistant, textToSpeech};
